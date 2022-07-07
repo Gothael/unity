7df11afda27c9cef496911bcadfcc07777dcf2e9
Ken Talo <kenneth.talo@gmail.com>
2022-07-07 15:54:30 -0500

    Implementing bird script methods

--- cranky-birds/Assets/Scripts/Bird.cs
+++ cranky-birds/Assets/Scripts/Bird.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    Rigidbody2D rigidBody2d;
+    SpriteRenderer spriteRenderer;
+    
+    float origGravityScale;
+    Color origColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        origGravityScale = rigidBody2d.gravityScale;
+        origColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        rigidBody2d.gravityScale = -origGravityScale;
+        spriteRenderer.color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        rigidBody2d.gravityScale = origGravityScale;
+        spriteRenderer.color = origColor; ;
     }
 }
