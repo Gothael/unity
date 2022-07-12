ee4cf7cdb3a229d3129b7acea795394f4a819a46
Ken Talo <kenneth.talo@gmail.com>
2022-07-12 15:56:10 -0500

    Challenge - Keyboard controls !!!

--- delivery-driver/Assets/Scripts/Driver.cs
+++ delivery-driver/Assets/Scripts/Driver.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
     private void Start()
     {
-        
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(new Vector3(0, moveAmount));
     }
 }
